25158d41da9b404a4f2c819d94f254c11fa7d119
GM <gmanev@vmware.com>
2017-10-17 17:56:40 +0300

    launching the Ethereum modules by external script

--- launch.sh
+++ launch.sh
@@ -1,11 +1,20 @@
 #/bin/sh
+#TODO pass it down to Ethereum
+ethPort=8545
+
+./launch_eth.sh
+if [ $? -ne 0 ];
+then
+    echo "Launching Ethereum nodes failed"
+    exit 1
+fi
+
 echo "Starting Redis ...."
 /etc/init.d/redis-server start
 echo "Started Redis"
-echo "Starting the UI server ..."
 
-ethPort=8545
-./ethjsonrpc extClientsConfig.xml &
 #TODO figure out the IP address of this machine
 #172.17.0.2 is by default the IP of the first container on the bridge network - it is currently used by the local Docker registry container
+
+echo "Starting the UI server ..."
 ./blockparty 172.17.0.3 8080 ${ethPort}
